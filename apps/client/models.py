41e5ff0962ca0e9c68418fea5f8a1c78b1fc9dbe
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 14:44:24 +0300

    created Client model

--- apps/client/models.py
+++ apps/client/models.py
@@ -3,16 +3,35 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Client(models.Model):
-    phone_number = PhoneNumberField(unique=True)
-    operator_code = models.CharField(max_length=3, rel="client_filter_tag")
+    phone_number = PhoneNumberField(
+        unique=True,
+        verbose_name="Номер телефона",
+    )
+    mobile_operator_code = models.ForeignKey(
+        to="MobileOperatorCode",
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        verbose_name="Мобильный код оператора",
+    )
     tag = models.ForeignKey(
         to="Tag",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
+        verbose_name="Тэг",
     )
-    timezone = ...
+    timezone = models.CharField(
+        max_length=50,
+        verbose_name="Часовой пояс",
+    )
 
 
 class Tag(models.Model):
-    client_filter_tag = models.CharField(max_length=255)
+    client_filter_tag = models.CharField(
+        max_length=50,
+    )
+
+
+class MobileOperatorCode(models.Model):
+    operator_code = models.PositiveSmallIntegerField()
