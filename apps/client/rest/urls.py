05324db7fab5e4289c14a280c530c89161d80de7
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 22:22:03 +0300

    added router.urls for client

--- apps/client/rest/urls.py
+++ apps/client/rest/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from . import views
+
+router = routers.SimpleRouter()
+
+router.register("clients", views.ClientViewSet, basename="clients")
 
 app_name = "client"
 
-urlpatterns = []
+urlpatterns = [
+    path("", include(router.urls)),
+]
