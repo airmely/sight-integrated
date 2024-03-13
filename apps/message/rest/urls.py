a57914142f2ce20288d16ef2be36f2ae90c2604e
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-14 00:03:05 +0300

    register router and append in urlpatterns

--- apps/message/rest/urls.py
+++ apps/message/rest/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from apps.message.rest.views import MessageViewSet
 
+router = routers.SimpleRouter()
+
+router.register("messages", MessageViewSet, basename="messages")
 
 app_name = "message"
 
-urlpatterns = []
+urlpatterns = [
+    path("", include(router.urls)),
+]
