b106fedb22e2af9612c712cbc501a032fb8f0521
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 23:36:17 +0300

    added field title for Mailing Model

--- apps/mailing/models.py
+++ apps/mailing/models.py
@@ -8,6 +8,20 @@ class Mailing(models.Model):
     end_datetime_mailing = models.DateTimeField(
         verbose_name="Окончание рассылки",
     )
+    title = models.CharField(
+        max_length=50,
+        blank=False,
+        null=True,
+        verbose_name="Тема сообщения",
+    )
     message = models.TextField(
         verbose_name="Сообщение",
     )
+
+    class Meta:
+        _splash = "Рассылк%s"
+        verbose_name = _splash % "а"
+        verbose_name_plural = _splash % "и"
+
+    def __str__(self):
+        return self.title
