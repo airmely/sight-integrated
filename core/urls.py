f18be3a2dec5183a4e0287ca5cfe462c67776f21
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 12:51:26 +0300

    included urls from apps

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("apps.client.rest.urls")),
-    path("api/", include("apps.message.rest.urls")),
-    path("api/", include("apps.mailing.rest.urls")),
+    path(
+        "api/",
+        include(
+            [
+                path("client/", include("apps.client.rest.urls")),
+                path("message/", include("apps.message.rest.urls")),
+                path("mailing/", include("apps.mailing.rest.urls")),
+            ]
+        ),
+    ),
 ]
