b7ec785eff55d7f1161782f302839b861bc581a5
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 23:37:42 +0300

    register Message Model in django-admin

--- apps/message/admin.py
+++ apps/message/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.message.models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
