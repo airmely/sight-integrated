2f768a9904ac7c696f7d86266eac38594ceb406c
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 22:22:17 +0300

    created ClientViewSet

--- apps/client/rest/views.py
+++ apps/client/rest/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
 
-# Create your views here.
+from apps.client.models import Client
+from apps.client.rest.serializers import ClientSerializer
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [permissions.AllowAny]
