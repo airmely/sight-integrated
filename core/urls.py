ad47d6fd651202d03f3896ca1c14cafe97946a07
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 15:28:36 +0300

    added OpenAPI docs

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from utils import SightIntegratedAPISchemaGenerator
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,5 +17,25 @@ urlpatterns = [
                 path("mailing/", include("apps.mailing.rest.urls")),
             ]
         ),
+    ),
+    path(
+        "api/docs/",
+        get_schema_view(
+            info=openapi.Info(
+                title="Sight-Integrated API",
+                default_version="v1",
+            ),
+            generator_class=SightIntegratedAPISchemaGenerator,
+        ).with_ui("swagger"),
+    ),
+    path(
+        "api/redoc/",
+        get_schema_view(
+            info=openapi.Info(
+                title="Sight-Integrated API",
+                default_version="v1",
+            ),
+            generator_class=SightIntegratedAPISchemaGenerator,
+        ).with_ui("redoc"),
     ),
 ]
