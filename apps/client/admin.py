10787d7a8897b0c17b84cedbc6eef9f85f235327
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-13 22:23:33 +0300

    added filter_horizontal in ClientAdmin

--- apps/client/admin.py
+++ apps/client/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import Client
+
+admin.site.unregister(Group)
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     readonly_fields = ["mobile_operator_code"]
+    filter_horizontal = ("tag",)
