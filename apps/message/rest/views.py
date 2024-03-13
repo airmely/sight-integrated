e0951eb234e5537c8751ab940dcd2e9f2fb1643b
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-14 00:03:21 +0300

    created MessageViewSet

--- apps/message/rest/views.py
+++ apps/message/rest/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .serializers import MessageSerializer
+from ..models import Message
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    @swagger_auto_schema(
+        operation_description="This endpoint allows administrators to perform CRUD operations on messages.",
+        responses={200: openapi.Response("Successful operation"), 400: "Bad request"},
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
