ce100de8bfe5eccf02a4d4449afd1fc268fc7e43
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-14 15:35:19 +0300

    added doc-string

--- apps/mailing/rest/views.py
+++ apps/mailing/rest/views.py
@@ -4,13 +4,18 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 from apps.mailing.rest.serializers import MailingMessageToClientsSerializer
 
 
 class ExternalServiceView(APIView):
-    permission_classes = [IsAuthenticated]
+    """
+    Отправляет сообщение через внешний сервис.
+    permission_classes = [IsAdminUser]
+    """
+
+    permission_classes = [permissions.IsAdminUser]
 
     @swagger_auto_schema(
         request_body=MailingMessageToClientsSerializer,
