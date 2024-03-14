cf2d6023ac99dcce6dcd38431c1bc8b875693202
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-14 13:32:24 +0300

    added an endpoint for requests to an external service

--- apps/mailing/rest/urls.py
+++ apps/mailing/rest/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
+from apps.mailing.rest.views import ExternalServiceView
 
 app_name = "mailing"
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "external-service/<int:msg_id>/",
+        ExternalServiceView.as_view(),
+        name="external-mail",
+    ),
+]
