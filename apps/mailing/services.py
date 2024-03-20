a13b68c0e4ab2c78082cec965533455726a01a20
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-20 15:47:13 +0300

    added function filter by client with tag and mob_op_code and function get interval time by client for different timezone

--- apps/mailing/services.py
+++ apps/mailing/services.py
@@ -1,30 +1,28 @@
-from typing import List, Optional
+from typing import List
+from pytz import timezone
+from datetime import datetime, timedelta
 
-import requests
-from django.conf import settings
+from django.db.models import Q
 
 from apps.client.models import Tag, MobileOperatorCode, Client
-from apps.message.models import Message
 
 
 def get_client_by_filter_of_tag_and_code_mobile_operator(
-    timezone: Optional[str] = None,
-    tags: Optional[List[Tag]] = None,
-    mobile_operator: Optional[MobileOperatorCode] = None,
-) -> Optional[List[Client]]:
-    clients = Client.objects.prefetch_related("tag__client_set").filter(
-        tags__in=tags,
-        mobile_operator_code__in=mobile_operator,
-        timezone=timezone,
+    tags: List[Tag],
+    mobile_operator: List[MobileOperatorCode],
+) -> List[Client]:
+    clients = (
+        Client.objects.filter(
+            Q(tag__in=tags) | Q(mobile_operator_code__in=mobile_operator)
+        )
+        .distinct()
+        .only("phone_number")
     )
     return clients
 
 
-def process_send_mailing_to_client(clients: List[Client], message: Message) -> None:
-    for client in clients:
-        headers = {"Authorization": f"Bearer {settings.API_TOKEN}"}
-        response = requests.post(
-            f"{settings.URL_EXTERNAL_SERVICE}{message.id}",
-            json=client,
-            headers=headers,
-        )
+def get_current_time(client_timezone: str) -> datetime:
+    now = datetime.now()
+    tz = timezone(client_timezone)
+    eta_time = tz.localize(now + timedelta(hours=1))
+    return eta_time
