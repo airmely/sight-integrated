2025afb678b3689ddc1345b6a5d9a6f753891995
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-23 11:51:04 +0300

    created signals, but it doesn't work correctly

--- apps/mailing/signals.py
+++ apps/mailing/signals.py
@@ -1,7 +1,8 @@
 import logging
 
-from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 from apps.mailing.models import Mailing
 from apps.mailing.services import get_current_time
 from apps.mailing.tasks import process_message_for_client
@@ -10,8 +11,8 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Mailing)
-def run_celery_task_on_mailing_creation(sender, instance, created, **kwargs):
+def run_celery_task_on_mailing_creation(sender, instance=None, created=None, **kwargs):
     logger.info(f"Mailing {instance.id} created")
     if created:
-        eta = get_current_time(instance.client_timezone.timezone)
-        process_message_for_client.delay(instance)
+        # eta = get_current_time(instance.client_timezone.timezone)
+        process_message_for_client(instance)
