1aa6374335628ce07a3dbfa52d4013f489e9d5a0
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-23 11:48:23 +0300

    register signals

--- apps/mailing/apps.py
+++ apps/mailing/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.mailing"
+
+    def ready(self):
+        from .signals import run_celery_task_on_mailing_creation
