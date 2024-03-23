f132aad52b69874725f3092a82d9482f32032797
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-23 11:52:05 +0300

    created serializer MailingViewSet

--- apps/mailing/rest/serializers.py
+++ apps/mailing/rest/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from phonenumber_field.validators import validate_international_phonenumber
 
+from apps.mailing.models import Mailing
+
 
 class MailingMessageToClientsSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     phone = serializers.CharField(validators=[validate_international_phonenumber])
     text = serializers.CharField()
+
+
+class MailingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mailing
+        fields = "__all__"
