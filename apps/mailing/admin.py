64788d63c78c819a3566b314c97051715c2edb7a
Airenik Melkonyan <melkonyanair@outlook.com>
2024-03-23 11:49:10 +0300

    created action on Mailing Admin

--- apps/mailing/admin.py
+++ apps/mailing/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 from apps.mailing.models import Mailing
+from apps.mailing.tasks import process_message_for_client
+
+
+def action_mailing_send_by_client(model, request, queryset):
+    process_message_for_client(queryset.first())
+
+
+action_mailing_send_by_client.short_description = "Send mailing by client"
 
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
     filter_horizontal = ("tag", "mobile_operator_code")
+    actions = [action_mailing_send_by_client]
